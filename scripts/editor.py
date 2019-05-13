2404b4d59670023e74384db8dd23b4fa8c3b4801
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-05-13 10:57:34 +0200

    Added docs

--- scripts/editor.py
+++ scripts/editor.py
@@ -4,12 +4,13 @@ import os
 
 
 @click.command()
-@click.option('--set', '-s', nargs=2, type=str, multiple=True)
-@click.option('--unset', '-u', nargs=1, type=str, multiple=True)
-@click.option('--add', '-a', nargs=2, type=str, multiple=True)
-@click.option('--remove', '-r', nargs=2, type=str, multiple=True)
+@click.option('--set', '-s', nargs=2, type=str, multiple=True, help="Set the attribute to the given value")
+@click.option('--unset', '-u', nargs=1, type=str, multiple=True, help="Remove the given attribute")
+@click.option('--add', '-a', nargs=2, type=str, multiple=True, help="Add a value to the given attribute")
+@click.option('--remove', '-r', nargs=2, type=str, multiple=True, help="Remove a value from the given attribute")
 @click.argument('person-id', type=int)
 def edit(set, unset, add, remove, person_id):
+    """Edit a single person"""
     file_id = '{:04d}'.format(person_id)
     filepath = '{0}.json'.format(os.path.join('content', 'people', file_id[:2], file_id))
     if os.path.exists(filepath):
