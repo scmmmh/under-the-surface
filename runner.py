1936bea232ac38b017f8fb879967eb19802547b4
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-05-15 23:26:30 +0200

    Added custom generation runner

---
+++ runner.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import signal
+import sys
+
+from time import sleep
+from subprocess import run, Popen, DEVNULL
+
+
+def notification_handler(a, b):
+    print('\nShutting down\n')
+
+signal.signal(signal.SIGINT, notification_handler)
+
+run(['rm', '-rf', 'output'])
+processes = []
+if len(sys.argv) > 1 and sys.argv[1] == '-r':
+    processes.append(Popen(['pelican', '-s', 'publishconf_en.py', '-r'], stderr=DEVNULL))
+    processes.append(Popen(['pelican', '-s', 'publishconf_de.py', '-r'], stderr=DEVNULL))
+    processes.append(Popen(['http-server', 'output']))
+else:
+    processes.append(Popen(['pelican', '-s', 'publishconf_en.py']))
+    processes.append(Popen(['pelican', '-s', 'publishconf_de.py']))
+
+live = True
+while live:
+    sleep(1)
+    live = False
+    for process in processes:
+        if process.poll() is None:
+            live = True
