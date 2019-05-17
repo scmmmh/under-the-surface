b574ec50aefeae89027938eb11591724f76ddbe9
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-05-17 09:15:01 +0200

    Updated menu CSS and JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     //autoprefixer = require('gulp-autoprefixer'),
-    //webpack = require('webpack-stream'),
+    concat = require('gulp-concat'),
     pump = require('pump');
 
 // Compile the CSS
@@ -22,11 +22,25 @@ gulp.task('styles', function(cb) {
     ], cb);
 });
 
-//gulp.task('scripts', gulp.series());
+gulp.task('scripts', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/what-input/dist/what-input.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js',
+            'node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min.js',
+            'node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js',
+            'node_modules/foundation-sites/dist/js/plugins/foundation.util.nest.min.js',
+            'node_modules/foundation-sites/dist/js/plugins/foundation.dropdownMenu.min.js',
+            'js/app.js'
+        ]),
+        concat('app.js'),
+        gulp.dest('theme/static/js')
+    ], cb);
+});
 
 // Generic tasks
-//gulp.task('default', gulp.parallel('styles', 'scripts'));
-gulp.task('default', gulp.parallel('styles'));
+gulp.task('default', gulp.parallel('styles', 'scripts'));
 
 gulp.task('watch', gulp.series('default', function(cb) {
     gulp.watch('styles/*.*', gulp.series('styles'));
