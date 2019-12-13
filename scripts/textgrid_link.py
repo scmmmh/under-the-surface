220447182383056247968f38c0a287b517712369
Mark Hall <mark.hall@work.room3b.eu>
2019-12-13 11:39:05 +0000

    Added progress bar

--- scripts/textgrid_link.py
+++ scripts/textgrid_link.py
@@ -46,25 +46,26 @@ def fetch_textgrid_search(title):
 @click.pass_context
 def load_textgrid_data(ctx):
     dbsession = ctx.obj['dbsession']
-    for person in dbsession.query(Person):
-        data = fetch_textgrid_search(person.title)
-        if data:
-            source = {'url': QUERY_URL.format(person.title),
-                      'label': 'TextGrid API',
-                      'timestamp': datetime.now()}
-            data = etree.fromstring(data)
-            if int(data.attrib['hits']) > 0:
-                for result in data:
-                    agent = get_xml_attribute(result, 'tg:object.tg:edition.tg:agent', ns=NAMESPACES)
-                    if agent is not None and agent.text == format_query_author(person.title) and 'role' in agent.attrib and agent.attrib['role'] == 'author':
-                        title = get_xml_attribute(result,
-                                                  'tg:object.tg:generic.tg:provided.tg:title.text()',
-                                                  ns=NAMESPACES)
-                        tgid = get_xml_attribute(result,
-                                                 'tg:object.tg:generic.tg:generated.tg:textgridUri.text()',
-                                                 ns=NAMESPACES)
-                        work = merge_work(dbsession, person, title, source)
-                        merge_work_property(dbsession, work, '{0}§provider'.format(tgid), {'label': 'TextGrid', 'value': 'https://textgridrep.org'}, source)
-                        merge_work_property(dbsession, work, '{0}§data'.format(tgid), {'value': ITEM_URL.format(tgid)}, source)
-                        merge_work_property(dbsession, work, '{0}§license'.format(tgid), {'label': 'CC-BY', 'value': 'cc-by'}, source)
-                        merge_work_property(dbsession, work, '{0}§license_url'.format(tgid), {'value': 'https://textgrid.de/Digitale-Bibliothek'}, source)
+    with click.progressbar(dbsession.query(Person), length=dbsession.query(Person).count(), label='Linking People') as bar:
+        for person in bar:
+            data = fetch_textgrid_search(person.title)
+            if data:
+                source = {'url': QUERY_URL.format(person.title),
+                          'label': 'TextGrid API',
+                          'timestamp': datetime.now()}
+                data = etree.fromstring(data)
+                if int(data.attrib['hits']) > 0:
+                    for result in data:
+                        agent = get_xml_attribute(result, 'tg:object.tg:edition.tg:agent', ns=NAMESPACES)
+                        if agent is not None and agent.text == format_query_author(person.title) and 'role' in agent.attrib and agent.attrib['role'] == 'author':
+                            title = get_xml_attribute(result,
+                                                      'tg:object.tg:generic.tg:provided.tg:title.text()',
+                                                      ns=NAMESPACES)
+                            tgid = get_xml_attribute(result,
+                                                     'tg:object.tg:generic.tg:generated.tg:textgridUri.text()',
+                                                     ns=NAMESPACES)
+                            work = merge_work(dbsession, person, title, source)
+                            merge_work_property(dbsession, work, '{0}§provider'.format(tgid), {'label': 'TextGrid', 'value': 'https://textgridrep.org'}, source)
+                            merge_work_property(dbsession, work, '{0}§data'.format(tgid), {'value': ITEM_URL.format(tgid)}, source)
+                            merge_work_property(dbsession, work, '{0}§license'.format(tgid), {'label': 'CC-BY', 'value': 'cc-by'}, source)
+                            merge_work_property(dbsession, work, '{0}§license_url'.format(tgid), {'value': 'https://textgrid.de/Digitale-Bibliothek'}, source)
